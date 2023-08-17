29e851cb9cd881f0860c0b3faf87f70c06a9c25b
Dmytro Ovchynnikov <dmytro.ovchynnikov@coderio.co>
2023-08-17 11:00:52 -0300

    TypeResolver

--- library/src/main/java/org/tybaco/types/resolver/TypeResolverResults.java
+++ library/src/main/java/org/tybaco/types/resolver/TypeResolverResults.java
@@ -23,18 +23,27 @@ package org.tybaco.types.resolver;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.jdt.internal.compiler.lookup.ArrayBinding;
+import org.eclipse.jdt.internal.compiler.lookup.BaseTypeBinding;
 import org.eclipse.jdt.internal.compiler.lookup.CompilationUnitScope;
+import org.eclipse.jdt.internal.compiler.lookup.IntersectionTypeBinding18;
 import org.eclipse.jdt.internal.compiler.lookup.MethodBinding;
+import org.eclipse.jdt.internal.compiler.lookup.ParameterizedTypeBinding;
+import org.eclipse.jdt.internal.compiler.lookup.ReferenceBinding;
 import org.eclipse.jdt.internal.compiler.lookup.TypeBinding;
 import org.eclipse.jdt.internal.compiler.lookup.TypeVariableBinding;
+import org.eclipse.jdt.internal.compiler.lookup.VoidTypeBinding;
+import org.eclipse.jdt.internal.compiler.lookup.WildcardBinding;
 import org.tybaco.types.resolver.Method.Arg;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
 import java.util.function.BiConsumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
 import static org.eclipse.jdt.internal.compiler.lookup.Scope.convertEliminatingTypeVariables;
 
@@ -50,6 +59,22 @@ public final class TypeResolverResults {
     public ResolvedType getType(String name) {
         var type = types.get(name);
         return new ResolvedType(type == null ? TypeBinding.VOID : type);
+    }
+
+    public ResolvedType typeOf(String... parts) {
+        var compoundName = new char[parts.length][];
+        for (int i = 0; i < parts.length; i++) {
+            compoundName[i] = parts[i].toCharArray();
+        }
+        return new ResolvedType(scope.getType(compoundName, parts.length));
+    }
+
+    public ResolvedType array(ResolvedType type, int dimension) {
+        return new ResolvedType(scope.createArrayType(type.type, dimension));
+    }
+
+    public ResolvedType boxed(ResolvedType type) {
+        return new ResolvedType(scope.boxing(type.type));
     }
 
     private static Method method(ResolvedType type, MethodBinding m) {
@@ -88,11 +113,40 @@ public final class TypeResolverResults {
     }
 
     public boolean isAssignable(ResolvedType from, ResolvedType to) {
-        if (to.type instanceof TypeVariableBinding b) {
-            var ground = convertEliminatingTypeVariables(b, b, b.rank, null);
-            return isAssignable(from, new ResolvedType(ground));
+        var grounded = ground(to.type);
+        return from.type.isBoxingCompatibleWith(grounded, scope);
+    }
+
+    private TypeBinding ground(TypeBinding b) {
+        if (b instanceof TypeVariableBinding t) {
+            return convertEliminatingTypeVariables(t, t, t.rank, null);
+        } else if (b instanceof ArrayBinding t) {
+            return new ArrayBinding(ground(t.leafComponentType), t.dimensions, t.environment());
+        } else if (b instanceof BaseTypeBinding) {
+            return b;
+        } else if (b instanceof ParameterizedTypeBinding t) {
+            return new ParameterizedTypeBinding(
+                    t.actualType(),
+                    stream(t.arguments).map(this::ground).toArray(TypeBinding[]::new),
+                    t.enclosingType(),
+                    t.environment()
+            );
+        } else if (b instanceof IntersectionTypeBinding18 t) {
+            return new IntersectionTypeBinding18(
+                    stream(t.intersectingTypes).map(e -> (ReferenceBinding) ground(e)).toArray(ReferenceBinding[]::new),
+                    scope.environment
+            );
+        } else if (b instanceof WildcardBinding t) {
+            return new WildcardBinding(
+                    t.genericType,
+                    t.rank,
+                    t.bound == null ? null : ground(t.bound),
+                    t.otherBounds == null ? null : stream(t.otherBounds).map(this::ground).toArray(TypeBinding[]::new),
+                    t.boundKind,
+                    scope.environment
+            );
         } else {
-            return from.type.isBoxingCompatibleWith(to.type, scope);
+            return b;
         }
     }
 
