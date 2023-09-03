83116a5175ca58ea7dcee5015b63d3b5e4003e66
Dmytro Ovchynnikov <dmytro.ovchynnikov@coderio.co>
2023-09-03 14:34:13 -0300

    Remove custom bootstrapper

--- ide/src/main/java/org/tybaco/ide/Ide.java
+++ ide/src/main/java/org/tybaco/ide/Ide.java
@@ -26,21 +26,9 @@ import org.tybaco.ide.splash.SplashStatus;
 import org.tybaco.logging.FastConsoleHandler;
 import org.tybaco.logging.LoggingManager;
 
-import java.io.File;
-import java.net.*;
-import java.nio.file.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.jar.JarFile;
 import java.util.logging.LogManager;
 
 import static java.lang.System.setProperty;
-import static java.nio.file.Files.copy;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static java.util.logging.Level.INFO;
-import static java.util.logging.Level.SEVERE;
 import static java.util.logging.LogManager.getLogManager;
 import static org.tybaco.ide.splash.Splash.renderSplash;
 import static org.tybaco.ide.splash.Splash.updateSplash;
@@ -51,19 +39,15 @@ public class Ide {
     renderSplash();
     initLogging();
     var logger = LogManager.getLogManager().getLogger("");
-    logger.info("Initializing classpath");
-    var urls = libUrls();
-    logger.log(INFO, "Classpath initialized: {0}", List.of(urls));
     updateSplash();
-    var classLoader = classLoader(urls);
     logger.info("Preparing UI");
-    bootstrapSplash(classLoader);
+    bootstrapSplash(Thread.currentThread().getContextClassLoader());
     logger.info("UI prepared");
-    invokeMain(classLoader, args);
+    invokeMain(Thread.currentThread().getContextClassLoader(), args);
     logger.info("UI launched");
   }
 
-  private static void bootstrapSplash(URLClassLoader classLoader) throws Exception {
+  private static void bootstrapSplash(ClassLoader classLoader) throws Exception {
     updateSplash();
     var mainClass = classLoader.loadClass("org.tybaco.ui.Main");
     var updateSplash = mainClass.getField("updateSplash");
@@ -77,119 +61,15 @@ public class Ide {
     processorUpdateSplash.set(null, (Runnable) () -> Splash.updateSplash(false));
   }
 
-  private static void invokeMain(URLClassLoader classLoader, String... args) throws Exception {
+  private static void invokeMain(ClassLoader classLoader, String... args) throws Exception {
     var mainClass = classLoader.loadClass("org.tybaco.ui.Main");
     var mainMethod = mainClass.getMethod("main", String[].class);
     mainMethod.invoke(null, (Object) args);
-  }
-
-  private static URL[] libUrls() throws Exception {
-    var excludedLibs = new TreeMap<String, String>();
-    try (var jarFile = new JarFile(new File(Ide.class.getProtectionDomain().getCodeSource().getLocation().toURI()), false)) {
-      var manifest = jarFile.getManifest();
-      var classPath = manifest.getMainAttributes().getValue("Class-Path");
-      for (var entry : classPath.split(" ")) {
-        if (entry.startsWith("lib/") && entry.endsWith(".jar")) {
-          excludedLibs.put(entry.substring("lib/".length()), null);
-        }
-      }
-    }
-    var libPath = Path.of(Ide.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent().resolve("lib");
-    var list = new LinkedList<URL>();
-    try (var ds = Files.newDirectoryStream(libPath, "*.jar")) {
-      var classifier = classifier();
-      var logger = LogManager.getLogManager().getLogger("");
-      try (var executor = executor()) {
-        var futures = new LinkedList<Future<URL>>();
-        for (var lib : ds) {
-          var fn = lib.getFileName().toString();
-          if (!excludedLibs.containsKey(fn)) {
-            list.addFirst(lib.toUri().toURL());
-          }
-          if (fn.startsWith("javafx-")) {
-            futures.add(executor.submit(() -> {
-              var j = fn.indexOf('-', "javafx-".length() + 1);
-              var artifact = fn.substring(0, j);
-              var version = fn.substring(j + 1, fn.length() - 4);
-              var base = "https://maven-central.storage.googleapis.com/maven2/org/openjfx/" + artifact;
-              var fxUrl = base + "/" + version + "/" + artifact + "-" + version + "-" + classifier + ".jar";
-              var url = URL.of(new URI(fxUrl), null);
-              var tempFile = Files.createTempFile("fx", ".jar");
-              try (var is = url.openStream()) {
-                copy(is, tempFile, REPLACE_EXISTING);
-              }
-              logger.log(INFO, "{0} copied", url);
-              return tempFile.toUri().toURL();
-            }));
-          }
-        }
-        for (var future : futures) {
-          try {
-            list.addLast(future.get());
-          } catch (Exception e) {
-            logger.log(SEVERE, "I/O error", e);
-          }
-        }
-      }
-    }
-    return list.toArray(URL[]::new);
-  }
-
-  private static String classifier() {
-    var osArch = System.getProperty("os.arch");
-    var osName = System.getProperty("os.name");
-    if (osName.equalsIgnoreCase("linux")) {
-      return osArch.equals("aarch64") ? "linux-aarch64" : "linux";
-    } else if (osName.equalsIgnoreCase("windows")) {
-      return "win";
-    } else {
-      return osArch.equals("aarch64") ? "mac-aarch64" : "mac";
-    }
   }
 
   private static void initLogging() {
     setProperty("java.util.logging.manager", LoggingManager.class.getName());
     var rootLogger = getLogManager().getLogger("");
     rootLogger.addHandler(new FastConsoleHandler());
-  }
-
-  private static ThreadPoolExecutor executor() {
-    var counter = new AtomicInteger();
-    var threadGroup = new ThreadGroup("fx-libs");
-    var threadFactory = (ThreadFactory) r -> {
-      var thread = new Thread(threadGroup, r, "t" + counter.getAndIncrement());
-      thread.setDaemon(true);
-      return thread;
-    };
-    var policy = new ThreadPoolExecutor.CallerRunsPolicy();
-    var executor = new ThreadPoolExecutor(16, 16, 1L, SECONDS, new SynchronousQueue<>(), threadFactory, policy);
-    executor.allowCoreThreadTimeOut(true);
-    return executor;
-  }
-
-  private static URLClassLoader classLoader(URL[] urls) {
-    var logger = LogManager.getLogManager().getLogger("");
-    var classLoader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
-    Thread.currentThread().setContextClassLoader(classLoader);
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      var closeFiles = (AutoCloseable) () -> {
-        var location = Ide.class.getProtectionDomain().getCodeSource().getLocation();
-        var mainJarDir = Path.of(location.toURI()).getParent();
-        for (var url : urls) {
-          var path = Path.of(url.toURI());
-          if (!path.startsWith(mainJarDir)) {
-            if (Files.deleteIfExists(path)) {
-              logger.log(INFO, "Deleted {0}", path);
-            }
-          }
-        }
-      };
-      try (closeFiles) {
-        classLoader.close();
-      } catch (Throwable e) {
-        logger.log(SEVERE, "I/O error", e);
-      }
-    }));
-    return classLoader;
   }
 }
