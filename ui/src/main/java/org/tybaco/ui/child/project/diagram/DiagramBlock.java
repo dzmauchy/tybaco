4bbf343d71dd4b65df2154380bf743db55b7e8f4
Dmytro Ovchynnikov <dmytro.ovchynnikov@coderio.co>
2023-10-12 07:32:41 -0300

    MetaTree

--- ui/src/main/java/org/tybaco/ui/child/project/diagram/DiagramBlock.java
+++ ui/src/main/java/org/tybaco/ui/child/project/diagram/DiagramBlock.java
@@ -10,20 +10,23 @@ package org.tybaco.ui.child.project.diagram;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
+import javafx.application.Platform;
 import org.tybaco.editors.icon.Icons;
 import org.tybaco.ui.model.Block;
 import org.tybaco.ui.model.Link;
+
+import static java.util.Collections.binarySearch;
 
 public final class DiagramBlock extends AbstractDiagramBlock {
 
@@ -40,7 +43,7 @@ public final class DiagramBlock extends AbstractDiagramBlock {
     outputs.getChildren().clear();
     diagram.blockCache.blockById(block.factoryId).ifPresent(b -> {
       factory.setGraphic(Icons.icon(diagram.classpath.getClassLoader(), b.icon(), 32));
-      b.forEachInput((spot, i) -> inputs.getChildren().add(new DiagramBlockInput(this, i, spot)));
+      b.forEachInput((spot, i) -> inputs.getChildren().add(new DiagramBlockInput(this, i, spot, -1)));
       b.forEachOutput((spot, o) -> outputs.getChildren().add(new DiagramBlockOutput(this, o, spot)));
     });
   }
@@ -49,7 +52,18 @@ public final class DiagramBlock extends AbstractDiagramBlock {
     for (var n : inputs.getChildren()) {
       if (n instanceof DiagramBlockInput i) {
         if (i.block.block.id == link.in.blockId && i.spot.equals(link.in.spot)) {
-          i.onLink(link, added);
+          if (i.index == link.index) {
+            i.onLink(link, added);
+          } else {
+            Platform.runLater(() -> {
+              var ni = new DiagramBlockInput(i.block, i.input, i.spot, link.index);
+              var k = binarySearch(inputs.getChildren(), link, DiagramBlockInput::internalCompare);
+              if (k < 0) {
+                inputs.getChildren().add(-(k + 1), ni);
+                ni.onLink(link, added);
+              }
+            });
+          }
         }
       }
     }
--- ui/src/main/java/org/tybaco/ui/child/project/diagram/DiagramBlockInput.java
+++ ui/src/main/java/org/tybaco/ui/child/project/diagram/DiagramBlockInput.java
@@ -23,37 +23,38 @@ package org.tybaco.ui.child.project.diagram;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import org.tybaco.editors.icon.Icons;
 import org.tybaco.editors.model.LibInput;
 import org.tybaco.ui.model.Connector;
 import org.tybaco.ui.model.Link;
 
 import static java.util.Collections.binarySearch;
-import static java.util.Comparator.comparingInt;
-import static org.tybaco.editors.icon.Icons.icon;
 import static org.tybaco.ui.child.project.diagram.DiagramSpotPoints.installSpotPointMonitoring;
 
-public final class DiagramBlockInput extends BorderPane {
+public final class DiagramBlockInput extends Button {
 
   public final DiagramBlock block;
   public final LibInput input;
   public final String spot;
+  public final int index;
   public final Connector inp;
-  private final VBox vectorInputs;
-  private final Button inputButton;
+  public Link link;
 
-  public DiagramBlockInput(DiagramBlock block, LibInput input, String spot) {
+  public DiagramBlockInput(DiagramBlock block, LibInput input, String spot, int index) {
     this.block = block;
     this.input = input;
     this.spot = spot;
+    this.index = index;
     this.inp = new Connector(block.block.id, spot);
-    setTop(inputButton = new Button(null, icon(classLoader(), input.icon(), 20)));
-    inputButton.setFocusTraversable(false);
-    inputButton.setTooltip(DiagramTooltips.tooltip(classLoader(), input));
-    inputButton.setOnAction(this::onButton);
-    setCenter(vectorInputs = new VBox());
-    vectorInputs.setFillWidth(true);
+    if (index < 0 || !input.vector()) {
+      setGraphic(Icons.icon(classLoader(), input.icon(), 20));
+    } else {
+      setText(Integer.toString(index));
+    }
+    setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    setTooltip(DiagramTooltips.tooltip(classLoader(), input));
+    setFocusTraversable(false);
+    setOnAction(this::onButton);
   }
 
   private ClassLoader classLoader() {
@@ -62,31 +63,12 @@ public final class DiagramBlockInput extends BorderPane {
 
   public void onLink(Link link, boolean added) {
     if (added) {
-      inputButton.setUnderline(true);
-      if (link.index >= 0) {
-        var b = new Button(Integer.toString(link.index));
-        b.setFocusTraversable(false);
-        b.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        b.setUserData(link);
-        b.setOnAction(this::onVectorButton);
-        var i = binarySearch(vectorInputs.getChildren(), b, comparingInt(n -> ((Link) n.getUserData()).index));
-        if (i < 0) {
-          vectorInputs.getChildren().add(-(i + 1), b);
-          installSpotPointMonitoring(block.diagram.blocks, b, DiagramSpotPoints::inputSpot, link.inpSpot::bind);
-        }
-      } else {
-        installSpotPointMonitoring(block.diagram.blocks, inputButton, DiagramSpotPoints::inputSpot, link.inpSpot::bind);
-      }
+      setUnderline(true);
+      installSpotPointMonitoring(block.diagram.blocks, this, DiagramSpotPoints::inputSpot, link.inpSpot::bind);
+      this.link = link;
     } else {
-      if (link.index < 0) {
-        inputButton.setUnderline(false);
-        inputButton.setUserData(null);
-      } else {
-        vectorInputs.getChildren().removeIf(n -> link.equals(n.getUserData()));
-        if (vectorInputs.getChildren().isEmpty()) {
-          inputButton.setUnderline(false);
-        }
-      }
+      setUnderline(false);
+      this.link = null;
     }
   }
 
@@ -97,29 +79,41 @@ public final class DiagramBlockInput extends BorderPane {
     } else {
       co.setSelected(false);
       var out = new Connector(co.block.block.id, co.spot);
-      if (input.vector()) {
-        var i = vectorInputs.getChildren().stream().mapToInt(n -> ((Link) n.getUserData()).index).max().orElse(-1) + 1;
-        block.diagram.project.links.add(new Link(out, inp, i));
+      if (index < 0) {
+        var nextIndex = block.inputs.getChildren().stream()
+          .map(n -> (DiagramBlockInput) n)
+          .filter(i -> spot.equals(i.spot))
+          .mapToInt(i -> i.index)
+          .max()
+          .orElseThrow() + 1;
+        var b = new DiagramBlockInput(block, input, spot, nextIndex);
+        var i = binarySearch(block.inputs.getChildren(), b, DiagramBlockInput::internalCompare);
+        block.inputs.getChildren().add(-(i + 1), b);
+        block.diagram.project.links.add(new Link(out, inp, nextIndex));
       } else {
-        block.diagram.project.links.removeIf(l -> l.in.equals(inp));
-        block.diagram.project.links.add(new Link(out, inp));
+        if (link != null) block.diagram.project.links.remove(link);
+        block.diagram.project.links.add(new Link(out, inp, index));
       }
       block.diagram.currentOutput = null;
     }
   }
 
-  private void onVectorButton(ActionEvent event) {
-    var co = block.diagram.currentOutput;
-    if (co == null) {
-
+  static int internalCompare(Object o1, Object o2) {
+    if (o1 instanceof DiagramBlockInput i1 && o2 instanceof DiagramBlockInput i2) {
+      var c = i1.spot.compareTo(i2.spot);
+      if (c != 0) return c;
+      return Integer.compare(i1.index, i2.index);
+    } else if (o1 instanceof DiagramBlockInput i1 && o2 instanceof Link l2) {
+      var c = i1.spot.compareTo(l2.in.spot);
+      if (c != 0) return c;
+      return Integer.compare(i1.index, l2.index);
     } else {
-      co.setSelected(false);
-      var button = (Button) event.getSource();
-      var out = new Connector(co.block.block.id, co.spot);
-      var link = (Link) button.getUserData();
-      block.diagram.project.links.removeIf(l -> l.equals(link));
-      block.diagram.project.links.add(new Link(out, inp, link.index));
-      block.diagram.currentOutput = null;
+      return 0;
     }
+  }
+
+  @Override
+  public String toString() {
+    return block.block.id + "." + spot + "[" + index + "]";
   }
 }
