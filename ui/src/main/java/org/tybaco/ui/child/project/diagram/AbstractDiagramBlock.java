cd621ee6793ee5d44f79cb750dbb0f5a0780fd8c
Dmytro Ovchynnikov <dmytro.ovchynnikov@coderio.co>
2023-10-17 20:14:25 -0300

    MetaTree

--- ui/src/main/java/org/tybaco/ui/child/project/diagram/AbstractDiagramBlock.java
+++ ui/src/main/java/org/tybaco/ui/child/project/diagram/AbstractDiagramBlock.java
@@ -10,12 +10,12 @@ package org.tybaco.ui.child.project.diagram;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -39,9 +39,9 @@ abstract class AbstractDiagramBlock extends BorderPane {
 
   final Diagram diagram;
   final Block block;
-  final Label blockId = new Label();
+  private final Label blockIdLabel = new Label();
   final Label blockName = new Label();
-  final HBox title = new HBox(3d, blockId, new Separator(VERTICAL), blockName);
+  final HBox title = new HBox(3d, blockIdLabel, new Separator(VERTICAL), blockName);
   final Label factory = new Label();
   final VBox inputs = new VBox(3);
   final VBox outputs = new VBox(3);
@@ -54,32 +54,48 @@ abstract class AbstractDiagramBlock extends BorderPane {
     this.block = block;
     setBackground(new Background(new BackgroundFill(Color.gray(0.2), new CornerRadii(5), Insets.EMPTY)));
     setBorder(new Border(new BorderStroke(WHITE, SOLID, new CornerRadii(5d), new BorderWidths(2d))));
-    factory.setPadding(new Insets(4d));
-    blockName.textProperty().bind(block.name);
-    blockName.setFont(Font.font(null, FontWeight.BOLD, 12));
-    blockName.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    blockName.setAlignment(Pos.BASELINE_CENTER);
-    blockId.setText(Integer.toString(block.id));
-    blockId.setAlignment(Pos.BASELINE_CENTER);
-    HBox.setHgrow(blockName, Priority.ALWAYS);
     setLayoutX(block.x.get());
     setLayoutY(block.y.get());
     block.x.bind(layoutXProperty());
     block.y.bind(layoutYProperty());
+    configureFactoryAndBlockId();
+    configureInputsAndOutputs();
+    configureBlockName();
+    configureTitle();
+    addEventHandler(MouseEvent.MOUSE_MOVED, this::onMouseMoved);
+    addEventHandler(MouseEvent.MOUSE_DRAGGED, this::onMouseDragged);
+  }
+
+  private void configureTitle() {
     setTop(title);
-    setCenter(factory);
+    title.setBorder(new Border(new BorderStroke(WHITE, SOLID, CornerRadii.EMPTY, new BorderWidths(0, 0, 2, 0))));
+    title.setPadding(new Insets(5d));
+    title.setFillHeight(true);
+    title.setStyle("-fx-background-color: linear-gradient(to top, black, transparent);");
+  }
+
+  private void configureBlockName() {
+    blockName.textProperty().bind(block.name);
+    blockName.setFont(Font.font(null, FontWeight.BOLD, 12));
+    blockName.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    blockName.setAlignment(Pos.BASELINE_CENTER);
+    HBox.setHgrow(blockName, Priority.ALWAYS);
+  }
+
+  private void configureInputsAndOutputs() {
     setLeft(inputs);
     setRight(outputs);
     inputs.setAlignment(Pos.CENTER);
     outputs.setAlignment(Pos.CENTER);
     inputs.setFillWidth(true);
     outputs.setFillWidth(true);
-    title.setBorder(new Border(new BorderStroke(WHITE, SOLID, CornerRadii.EMPTY, new BorderWidths(0, 0, 2, 0))));
-    title.setPadding(new Insets(5d));
-    title.setFillHeight(true);
-    title.setStyle("-fx-background-color: linear-gradient(to top, black, transparent);");
-    addEventHandler(MouseEvent.MOUSE_MOVED, this::onMouseMoved);
-    addEventHandler(MouseEvent.MOUSE_DRAGGED, this::onMouseDragged);
+  }
+
+  private void configureFactoryAndBlockId() {
+    factory.setPadding(new Insets(4d));
+    setCenter(factory);
+    blockIdLabel.setText(Integer.toString(block.id));
+    blockIdLabel.setAlignment(Pos.BASELINE_CENTER);
   }
 
   protected void onMouseMoved(MouseEvent event) {
@@ -96,27 +112,23 @@ abstract class AbstractDiagramBlock extends BorderPane {
       bounds.getWidth(),
       bounds.getHeight()
     );
-    if (checkConstraints(bb)) {
-      setLayoutX(getLayoutX() + event.getX() - bx);
-      setLayoutY(getLayoutY() + event.getY() - by);
-    }
+    checkConstraints(bb);
+    setLayoutX(getLayoutX() + event.getX() - bx);
+    setLayoutY(getLayoutY() + event.getY() - by);
   }
 
-  private boolean checkConstraints(Bounds bounds) {
+  private void checkConstraints(Bounds bounds) {
     for (var node : diagram.blocks.getChildren()) {
-      if (node != this) {
-        var b = node.getBoundsInParent();
-        var bb = new BoundingBox(
-          b.getMinX() - 35d,
-          b.getMinY() - 35d,
-          b.getWidth() + 70d,
-          b.getHeight() + 70d
-        );
-        if (bb.intersects(bounds)) {
-          return false;
-        }
+      if (node == this) continue;
+      var b = node.getBoundsInParent();
+      var bb = new BoundingBox(
+        b.getMinX() - 35d,
+        b.getMinY() - 35d,
+        b.getWidth() + 70d,
+        b.getHeight() + 70d
+      );
+      if (bb.intersects(bounds)) {
       }
     }
-    return true;
   }
 }
